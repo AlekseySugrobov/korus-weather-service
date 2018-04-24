5e76032c8ac188ee934599ef4ba231a83173e8cb
winzdoor <aleksey.sugrobov@gmail.com>
2018-04-24 19:16:42 +0500

    spring security

--- src/main/java/ru/sugrobov/weather/WeatherServiceApplication.java
+++ src/main/java/ru/sugrobov/weather/WeatherServiceApplication.java
@@ -2,6 +2,11 @@ package ru.sugrobov.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -9,5 +14,19 @@ public class WeatherServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WeatherServiceApplication.class, args);
+  }
+
+  @Configuration
+  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+          .httpBasic()
+          .and()
+          .authorizeRequests()
+          .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+          .anyRequest().authenticated();
+    }
   }
 }
